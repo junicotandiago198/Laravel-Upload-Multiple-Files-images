d12d6ba28bf63a0c4c29a19588d09434a6a52dfa
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-06-27 19:48:40 +0700

    setting routing, forelse data, product controller pharsing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
             <h3>Add a Product</h3>
             <form action="#" method="POST" enctype="multipart/form-data">
                 @csrf
-                <div class="form-group">
+                <div class="form-group mt-4">
                     <label for="title">Product Title:</label>
                     <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" placeholder="Enter product title">
                     <span class="text-danger">
@@ -14,7 +14,7 @@
                         @enderror
                     </span>
                 </div>
-                <div class="form-group">
+                <div class="form-group mt-4">
                     <label for="price">Product Price:</label>
                     <input type="number" step="any" min="1" class="form-control @error('price') is-invalid @enderror" name="price" placeholder="Enter product price">
                     <span class="text-danger">
@@ -34,6 +34,35 @@
         </div>
         <div class="col-md-6">
             <h3>Products</h3>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Product Title</th>
+                        <th>Price</th>
+                        <th>Total Image</th>
+                        <th>View Image</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php $i=1; @endphp
+                    @forelse ($products as $product)
+                        <tr>
+                            <td>{{ $i++; }}</td>
+                            <td>{{ $product->title }}</td>
+                            <td>${{ $product->price }}</td>
+                            <td>10</td>
+                            <td>
+                                <a href="#" class="btn btn-outline-dark">View</a>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="5" class="text-center">No products yet!</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
